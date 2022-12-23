194fd155a2d0b97acbca9f9470ff43430064a7da
rishabh-bijalwan12 <rishabhbijalwan19@gmail.com>
2022-12-23 14:02:49 +0530

    Update ProductCatalog.js

--- frontend/src/Components-erp/ProductCatalog.js
+++ frontend/src/Components-erp/ProductCatalog.js
@@ -333,6 +333,7 @@ function PlaceOrder() {
                                     Image
                                 </button> 
 
+
                             </div>
 
                         </div>
